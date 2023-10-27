6e795901ff059f599610563d0d020f5218a7f9cb
alfianadityads <alfianadityadwi@gmail.com>
2023-10-27 23:30:11 +0700

    feat: add update role with valid, invalid user_id, invalid req body and alphabet user_id

--- src/test/java/starter/cookit/admin/AdminAPI.java
+++ src/test/java/starter/cookit/admin/AdminAPI.java
@@ -4,11 +4,14 @@ import io.restassured.http.ContentType;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Step;
 import starter.cookit.auth.LoginAPI;
+import starter.cookit.users.UsersAPI;
 import starter.utils.Constant;
+
+import java.io.File;
 
 public class AdminAPI {
 
-    public static String GET_VERIFY_USER = Constant.BASE_URL + "/users/listverify";
+    public static String GET_LIST_VERIFY_USER = Constant.BASE_URL + "/users/listverify";
 
     public static String PUT_UPDATE_USER_ROLE = Constant.BASE_URL + "/users/approval/{id}";
 
@@ -19,5 +22,36 @@ public class AdminAPI {
         SerenityRest.given()
                 .header("Authorization", "Bearer " + ADMIN_TOKEN)
                 .contentType(ContentType.JSON);
+    }
+
+    @Step("Update user role with valid user_id and valid reqBody")
+    public static void setPutUpdateUserRole(File json) {
+        String ADMIN_TOKEN = LoginAPI.getAdminToken();
+        String USER_REQ_VERIFY_ID = UsersAPI.setGetUserIdByAdmin();
+        SerenityRest.given()
+                .header("Authorization", "Bearer " + ADMIN_TOKEN)
+                .contentType(ContentType.JSON)
+                .pathParam("id", USER_REQ_VERIFY_ID)
+                .body(json);
+    }
+
+    @Step("Update user role with exceed user_id and valid reqBody")
+    public static void setUpdateRoleExceedID(String userID, File json) {
+        String ADMIN_TOKEN = LoginAPI.getAdminToken();SerenityRest.given()
+                .header("Authorization", "Bearer " + ADMIN_TOKEN)
+                .contentType(ContentType.JSON)
+                .pathParam("id", userID)
+                .body(json);
+    }
+
+    @Step("Update users role for verify user with valid user_id and invalid body")
+    public static void setUpdateRoleInvalidReqBody(File json) {
+        String ADMIN_TOKEN = LoginAPI.getAdminToken();
+        String USER_REQ_VERIFY_ID = UsersAPI.setGetUserIdByAdmin();
+        SerenityRest.given()
+                .header("Authorization", "Bearer " + ADMIN_TOKEN)
+                .contentType(ContentType.JSON)
+                .pathParam("id", USER_REQ_VERIFY_ID)
+                .body(json);
     }
 }
--- src/test/java/starter/cookit/users/UsersAPI.java
+++ src/test/java/starter/cookit/users/UsersAPI.java
@@ -3,6 +3,7 @@ package starter.cookit.users;
 import io.restassured.http.ContentType;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Step;
+import starter.cookit.admin.AdminAPI;
 import starter.cookit.auth.LoginAPI;
 import starter.utils.Constant;
 
@@ -14,6 +15,15 @@ public class UsersAPI {
     public static String GET_LIST_USER_FOLLOWING = Constant.BASE_URL + "/users/following";
 
     public static String POST_REQUEST_UPGRADE_ACCOUNT = Constant.BASE_URL + "/users/upgrade";
+
+    @Step("Get user id by admin")
+    public static String setGetUserIdByAdmin(){
+        String ADMIN_TOKEN = LoginAPI.getAdminToken();
+        return SerenityRest.given().header("Authorization", "Bearer " + ADMIN_TOKEN)
+                .contentType(ContentType.JSON)
+                .when().get(AdminAPI.GET_LIST_VERIFY_USER)
+                .then().extract().response().jsonPath().getString("data[0].id");
+    }
 
     @Step("Get user by user id")
     public static void setGetUserById(String id){
