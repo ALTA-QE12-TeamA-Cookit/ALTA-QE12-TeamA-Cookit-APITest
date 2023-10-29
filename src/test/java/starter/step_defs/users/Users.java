220139dca3e9861db4be1f7557daf27a96c87da2
ilhm0 <muchsininilham@gmail.com>
2023-10-29 19:43:12 +0700

    completed feature/users part 1

--- src/test/java/starter/step_defs/users/Users.java
+++ src/test/java/starter/step_defs/users/Users.java
@@ -1,7 +1,6 @@
 package starter.step_defs.users;
 
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.rest.SerenityRest;
 import starter.cookit.users.UsersAPI;
